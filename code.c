144eedac08d423a72765901bda7776bf05a26d51
G0Hero <george13vladut@gmail.com>
2024-05-23 09:02:40 +0200

    Ajout de la fonction créer arbre et affichage très naïf de l'arbre

--- code.c
+++ code.c
@@ -5,16 +5,38 @@
 
 
 
-typedef struct TROU{
-    int deb;
-    int fin;
-} TROU;
 
 typedef struct M_ARBRE {
-    M_ARBRE *g;
-    TROU t;
-    M_ARBRE *d;
+    struct M_ARBRE *g;
+    int deb;
+    int fin;
+    struct M_ARBRE *d;
 } M_ARBRE;
 
 
+void M_arbre_creer(M_ARBRE **a) {
+    M_ARBRE *arbre = malloc(sizeof(M_ARBRE));
+    arbre->g = NULL;
+    arbre->d = NULL;
+    arbre->deb = 0;
+    arbre->fin = 30;
+    *a = arbre;
+}
 
+void M_ARBRE_afficher(M_ARBRE *a) {
+    if (a == NULL) {
+        printf("Arbre vide\n");
+    } else {
+        printf("Debut : %d\n", a->deb);
+        printf("Fin : %d\n", a->fin);
+        M_ARBRE_afficher(a->g);
+        M_ARBRE_afficher(a->d);
+    }
+}
+
+int main(){
+    M_ARBRE *a;
+    M_arbre_creer(&a);
+    M_ARBRE_afficher(a);
+    return 0;
+}
